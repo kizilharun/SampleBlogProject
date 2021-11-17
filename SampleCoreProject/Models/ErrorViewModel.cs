b288bf467aa45ea2c0bceb28b03a2e08e677b05e
Harun Kızıl <kizilharun@gmail.com>
2021-11-18 00:38:14 +0300

    first commit

---
+++ SampleCoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleCoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
