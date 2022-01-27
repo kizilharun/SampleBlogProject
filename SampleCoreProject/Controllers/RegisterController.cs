3417bf263934c7b5cc3d38253fa13c14aab7d743
Harun Kızıl <kizilharun@gmail.com>
2022-01-27 23:56:14 +0300

    crated register view

---
+++ SampleCoreProject/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleCoreProject.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer p)
+        {
+            return View();
+        }
+    }
+}
