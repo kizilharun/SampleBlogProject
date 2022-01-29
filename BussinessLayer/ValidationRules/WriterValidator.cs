1ac4d34656f244b6321f187c851bcfa7c3d45ae5
Harun Kızıl <kizilharun@gmail.com>
2022-01-29 23:57:24 +0300

    create fluent validation

---
+++ BussinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.ValidationRules
+{
+    public class WriterValidator:AbstractValidator<Writer> 
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("this field cannot be left blank !!");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("this field cannot be left blank !!");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("this field cannot be left blank !!");
+            RuleFor(x => x.WriterPassword).MaximumLength(2).WithMessage("cannot be less than two character !!");
+            
+        }
+    }
+}
--- SampleCoreProject/Controllers/RegisterController.cs
+++ SampleCoreProject/Controllers/RegisterController.cs
@@ -1,6 +1,8 @@
 ﻿using BussinessLayer.Concrete;
+using BussinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -22,10 +24,23 @@ namespace SampleCoreProject.Controllers
         [HttpPost]
         public IActionResult Index(Writer p)
         {
-            p.WriterStatus = true;
-            p.WriterAbout = "test";
-            wm.AddWriter(p);
-            return RedirectToAction("Index", "Blog");
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results = wv.Validate(p);
+            if (results.IsValid)
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "test";
+                wm.AddWriter(p);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
