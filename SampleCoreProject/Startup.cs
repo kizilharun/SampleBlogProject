0166fc36be96f2a80cad2ccc27e0643836eb93e1
Harun Kızıl <kizilharun@gmail.com>
2022-03-12 22:44:03 +0300

    created ErrorPage

--- SampleCoreProject/Startup.cs
+++ SampleCoreProject/Startup.cs
@@ -39,6 +39,7 @@ namespace SampleCoreProject
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStatusCodePagesWithReExecute("/ErrorPages/Error404","?code={0}");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
