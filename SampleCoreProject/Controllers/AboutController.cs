8ea78a01e6d3ea559d17fd88809a6c97f1f7001a
Harun Kızıl <kizilharun@gmail.com>
2022-03-14 23:53:51 +0300

    added SocialMediaAbout pw and set About View

--- SampleCoreProject/Controllers/AboutController.cs
+++ SampleCoreProject/Controllers/AboutController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,15 @@ namespace SampleCoreProject.Controllers
 {
     public class AboutController : Controller
     {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
         public IActionResult Index()
         {
             return View();
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+            var values = abm.GetList();
+            return PartialView(values);
         }
     }
 }
