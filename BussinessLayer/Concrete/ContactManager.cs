ed932dae6539a3eb77eef1657b2bd12038b44119
Harun Kızıl <kizilharun@gmail.com>
2022-07-19 23:16:42 +0300

    created contact page

---
+++ BussinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,26 @@
+﻿using BussinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        IContactDal _contactdal;
+
+        public ContactManager(IContactDal contactdal)
+        {
+            _contactdal = contactdal;
+        }
+
+        public void AddContact(Contact contact)
+        {
+            _contactdal.Insert(contact);
+        }
+    }
+}
