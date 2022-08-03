5cf5b236871d0c804ce494a61f56b974e7c11a7b
Harun Kızıl <kizilharun@gmail.com>
2022-08-03 23:49:54 +0300

    edited authentication

--- SampleCoreProject/Startup.cs
+++ SampleCoreProject/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -34,6 +35,9 @@ namespace SampleCoreProject
                 .Build();
                 config.Filters.Add(new AuthorizeFilter(policy));
             });
+
+            services.AddMvc();
+            services.AddAuthentication( CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(x => { x.LoginPath = "/Login/Index"; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
