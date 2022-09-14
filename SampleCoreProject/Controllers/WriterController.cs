5273b73afc92e76453d34b88c9c3052ddd29288a
Harun Kızıl <kizilharun@gmail.com>
2022-09-14 22:58:03 +0300

    edited writer panel's navbar and footer

--- SampleCoreProject/Controllers/WriterController.cs
+++ SampleCoreProject/Controllers/WriterController.cs
@@ -26,5 +26,15 @@ namespace SampleCoreProject.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
     }
 }
